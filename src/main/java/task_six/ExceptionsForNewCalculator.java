c9a701ede34b8872ccf7c27036f1df81bb9bf9fb
Kseniya Boldruf <marta31@mail.ru>
2022-04-18 21:23:01 +0300

    Задача №6 Исправлено

--- src/main/java/task_six/ExceptionsForNewCalculator.java
+++ src/main/java/task_six/ExceptionsForNewCalculator.java
@@ -2,6 +2,7 @@ package task_six;
 
 import task_four.Data;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionsForNewCalculator {
@@ -9,14 +10,19 @@ public class ExceptionsForNewCalculator {
 
         Scanner reader = new Scanner(System.in);
         System.out.print("Введите два числа через пробел: ");
-        int number1 = reader.nextInt();
-        int number2 = reader.nextInt();
+        try {
+            double number1 = reader.nextDouble();
+            double number2 = reader.nextDouble();
+
+
         System.out.print("Введите оператор (+, -, *, /): ");
 
         Data oper = new Data(number1,number2);
 
-        char op = reader.next().charAt(0);
-        switch (op) {
+            char op = reader.next().charAt(0);
+
+            switch (op) {
+
             case '+':
                 System.out.print("Резултат: " + oper.add(number1,number2));
                 break;
@@ -27,15 +33,21 @@ public class ExceptionsForNewCalculator {
                 System.out.print("Резултат: " + oper.times(number1,number2));
                 break;
             case '/':
-                try {
-                    System.out.print("Резултат: " + oper.div(number1,number2));
-                } catch (ArithmeticException e){
-                    System.out.print("На ноль делить нельзя!");
-                }
+               if (number2==0){
+                   try {
+                   throw new ArithmeticException("Деление на ноль.");
+                   }catch (ArithmeticException z){
+                       System.out.println("На 0 делить нельзя!");
+                   }
+               }else System.out.println("Резултат: " + oper.div(number1,number2));
                 break;
 
             default:
-                System.out.print("Некорректный оператор");
+                System.out.print("Некорректный оператор!");
+            }
+
+        } catch (InputMismatchException c){
+            System.out.print("Используйте только цифры!");
         }
         reader.close();
     }
