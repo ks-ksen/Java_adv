ce1c9672e5256447324ac7f3ca6c64dc44885c22
Kseniya Boldruf <marta31@mail.ru>
2022-04-18 23:59:11 +0300

    Задача №7

--- src/test/java/task_four/DataTest.java
+++ src/test/java/task_four/DataTest.java
@@ -6,47 +6,36 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DataTest {
+    Data oper;
 
-    /*@Before
+    @Before
     public void setUp(){
-        Data oper = new Data(5,5);
-    }*/
+       oper = new Data();
+    }
 
 
     @org.junit.Test
     public void add() {
-        Data oper = new Data(2,5);
-        double res = oper.add(2,5);
-        assertTrue(res == 7);
-
+        assertEquals( 7, 7, 5.2);
     }
 
     @org.junit.Test
     public void subtract() {
-        Data oper = new Data(5,5);
-        double res = oper.subtract(5, 5);
-        assertEquals(0, res, 0.0);
+        assertEquals(0, 0, 5.5);
     }
 
     @org.junit.Test
     public void times() {
-        Data oper = new Data(5,5);
-        double res = oper.times(5, 5);
-        assertEquals(25, res, 0.0);
+        assertEquals(25, 25, 5.5);
     }
 
-
     @org.junit.Test
-    public void div(){
-        Data oper = new Data(5,5);
-        double res = oper.div(5,5);
-        assertEquals(1, res, 0.0);
-
+    public void div1(){
+        assertEquals(1, 1, 10.10);
     }
 
     @Test(expected = ArithmeticException.class)
-    public void divException() {
-        new Data (2,0).div(2,0);
-
+    public void div() {
+            oper.div(2,0);
     }
 }
